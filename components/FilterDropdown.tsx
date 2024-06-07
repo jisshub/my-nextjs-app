6c6a42a6a15210598b886079faf6b5d8dc1a7ffe
Jissmon Jose <jissmon476@gmail.com>
2024-06-08 00:47:13 +0530

    COMMIT ALL CHANGES

--- components/FilterDropdown.tsx
+++ components/FilterDropdown.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Dropdown from 'react-bootstrap/Dropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -9,10 +11,10 @@ function FilterDropdown() {
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
-        <Dropdown.Item href="#/service">Service</Dropdown.Item>
-        <Dropdown.Item href="#/service-type">Service Type</Dropdown.Item>
-        <Dropdown.Item href="#/service-name">Service Name</Dropdown.Item>
-        <Dropdown.Item href="#/schedule">Schedule</Dropdown.Item>
+        <Dropdown.Item href="/service">Service</Dropdown.Item>
+        <Dropdown.Item href="/service-type">Service Type</Dropdown.Item>
+        <Dropdown.Item href="/service-name">Service Name</Dropdown.Item>
+        <Dropdown.Item href="/schedule">Schedule</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
