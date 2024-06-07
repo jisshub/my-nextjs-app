8eacab0e6843876cc254eb77d08ba1ff7ba22504
Jissmon Jose <jissmon476@gmail.com>
2024-06-08 00:38:20 +0530

    commit all

---
+++ components/FilterDropdown.tsx
@@ -0,0 +1,21 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function FilterDropdown() {
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Filter By
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/service">Service</Dropdown.Item>
+        <Dropdown.Item href="#/service-type">Service Type</Dropdown.Item>
+        <Dropdown.Item href="#/service-name">Service Name</Dropdown.Item>
+        <Dropdown.Item href="#/schedule">Schedule</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+export default FilterDropdown;
