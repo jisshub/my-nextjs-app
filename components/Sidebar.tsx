b1e77382181a00bb18ba57a1c0ebd651a4d8a393
Jissmon Jose <jissmon476@gmail.com>
2024-06-09 00:32:41 +0530

    commit changes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import PeopleIcon from '@mui/icons-material/People';
@@ -8,11 +8,23 @@ import CategoryIcon from '@mui/icons-material/Category';
 import Link from 'next/link';
 
 const Sidebar = () => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleListItemClick = (index: number) => {
+    setSelectedIndex(index);
+  };
+
   return (
     <Box sx={{ width: 250, bgcolor: 'background.paper', height: '100vh', borderRight: '1px solid #ddd' }}>
       <List component="nav">
         <Link href="/service/date-of-schedule" passHref>
-          <ListItem component="a">
+          <ListItem
+            button
+            component="a"
+            selected={selectedIndex === 0}
+            onClick={() => handleListItemClick(0)}
+            sx={{ backgroundColor: selectedIndex === 0 ? '#f0f0f0' : 'transparent' }}
+          >
             <ListItemIcon>
               <CalendarTodayIcon />
             </ListItemIcon>
@@ -20,7 +32,13 @@ const Sidebar = () => {
           </ListItem>
         </Link>
         <Link href="/service/people" passHref>
-          <ListItem component="a">
+          <ListItem
+            button
+            component="a"
+            selected={selectedIndex === 1}
+            onClick={() => handleListItemClick(1)}
+            sx={{ backgroundColor: selectedIndex === 1 ? '#f0f0f0' : 'transparent' }}
+          >
             <ListItemIcon>
               <PeopleIcon />
             </ListItemIcon>
@@ -28,7 +46,13 @@ const Sidebar = () => {
           </ListItem>
         </Link>
         <Link href="/service/products" passHref>
-          <ListItem component="a">
+          <ListItem
+            button
+            component="a"
+            selected={selectedIndex === 2}
+            onClick={() => handleListItemClick(2)}
+            sx={{ backgroundColor: selectedIndex === 2 ? '#f0f0f0' : 'transparent' }}
+          >
             <ListItemIcon>
               <CategoryIcon />
             </ListItemIcon>
