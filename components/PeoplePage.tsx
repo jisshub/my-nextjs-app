0d2824f0cdc7254f45977497b7e2d980caccf3ec
Jissmon Jose <jissmon476@gmail.com>
2024-06-09 19:02:56 +0530

    commit the changes

--- components/PeoplePage.tsx
+++ components/PeoplePage.tsx
@@ -1,26 +1,47 @@
 "use client";
 
-import React from 'react';
-import { Box, TextField } from '@mui/material';
+import React, { useState } from "react";
+import { Box, TextField, Button } from "@mui/material";
 import { CiSearch } from "react-icons/ci";
 
 const PeoplePage = () => {
+  const [searchText, setSearchText] = useState("");
+
+  const resetToDefault = () => {
+    setSearchText("");
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
+
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', p: 2, width: '100%' }}>
+    <Box sx={{ display: "flex", flexDirection: "column", p: 2, width: "100%" }}>
       <TextField
         fullWidth
         variant="outlined"
         placeholder="Search Payer or attendee name"
+        value={searchText}
+        onChange={handleSearchChange}
         InputProps={{
           startAdornment: (
-            <Box sx={{ display: 'flex', alignItems: 'center', mr: 1 }}>
+            <Box sx={{ display: "flex", alignItems: "center", mr: 1 }}>
               <CiSearch size={"30"} />
             </Box>
           ),
         }}
       />
+      <Box sx={{ display: "flex", justifyContent: "space-between", mt: 2 }}>
+        <Button variant="outlined" onClick={resetToDefault}>
+          Reset to Default
+        </Button>
+        <Button variant="contained" color="primary">
+          Apply
+        </Button>
+      </Box>
     </Box>
   );
 };
 
 export default PeoplePage;
+
