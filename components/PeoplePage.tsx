7ef201d51224eb22f1f72e8f6c82ac4cbe3e30d6
Jissmon Jose <jissmon476@gmail.com>
2024-06-09 20:12:02 +0530

    commit all the chnages

--- components/PeoplePage.tsx
+++ components/PeoplePage.tsx
@@ -1,8 +1,36 @@
 "use client";
 
 import React, { useState } from "react";
-import { Box, TextField, Button } from "@mui/material";
+import { Box, TextField, Button, Typography, List, ListItem, ListItemText, Chip, Checkbox } from "@mui/material";
 import { CiSearch } from "react-icons/ci";
+
+const users = [
+  {
+    id: 1,
+    name: "Abin",
+    tag: "Attendee"
+  },
+  {
+    id: 2,
+    name: "Ajith",
+    tag: "Payer"
+  },
+  {
+    id: 3,
+    name: "Alan",
+    tag: "Payer"
+  },
+  {
+    id: 4,
+    name: "Alex",
+    tag: "Attendee"
+  },
+  {
+    id: 5,
+    name: "Akash",
+    tag: "Attendee"
+  }
+];
 
 const PeoplePage = () => {
   const [searchText, setSearchText] = useState("");
@@ -14,6 +42,10 @@ const PeoplePage = () => {
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchText(event.target.value);
   };
+
+  const filteredUsers = users.filter(user =>
+    user.name.toLowerCase().includes(searchText.toLowerCase())
+  );
 
   return (
     <Box sx={{ display: "flex", flexDirection: "column", p: 2, width: "100%" }}>
@@ -39,9 +71,24 @@ const PeoplePage = () => {
           Apply
         </Button>
       </Box>
+      <Box sx={{ mt: 4 }}>
+        {searchText && (
+          <Typography variant="h6" gutterBottom>
+            Showing {filteredUsers.length} results matching '{searchText}'
+          </Typography>
+        )}
+        <List>
+          {filteredUsers.map(user => (
+            <ListItem key={user.id} sx={{ display: "flex", alignItems: "center" }}>
+              <Checkbox />
+              <ListItemText primary={user.name} />
+              <Chip label={user.tag} />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
     </Box>
   );
 };
 
 export default PeoplePage;
-
