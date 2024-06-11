1325079923c52a17c60beccb69029aa62135d5fa
Jissmon Jose <jissmon476@gmail.com>
2024-06-11 11:59:32 +0530

    COMMIT THE CHANGES

--- components/ProductsPage.tsx
+++ components/ProductsPage.tsx
@@ -7,6 +7,7 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  Button
 } from "@mui/material";
 import { users } from "../data/user";
 
@@ -17,38 +18,53 @@ const ProductsPage = () => {
   const serviceTypes = Array.from(new Set(users.map(user => user.type)));
   const statuses = Array.from(new Set(users.map(user => user.status)));
 
+  const resetToDefault = () => {
+    setServiceType("");
+    setStatus("");
+  };
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", p: 2, width: "100%" }}>
-      <FormControl fullWidth sx={{ mb: 2 }}>
-        <InputLabel>Service type</InputLabel>
-        <Select
-          value={serviceType}
-          onChange={(e) => setServiceType(e.target.value)}
-          label="Service type"
-        >
-          {serviceTypes.map(type => (
-            <MenuItem key={type} value={type}>
-              {type}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <FormControl fullWidth>
-        <InputLabel>Status</InputLabel>
-        <Select
-          value={status}
-          onChange={(e) => setStatus(e.target.value)}
-          label="Status"
-        >
-          {statuses.map(status => (
-            <MenuItem key={status} value={status}>
-              {status}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </Box>
-  );
+    <>
+        <Box sx={{ display: "flex", flexDirection: "column", p: 2, width: "100%" }}>
+            <FormControl fullWidth sx={{ mb: 2 }}>
+                <InputLabel>Service type</InputLabel>
+                <Select
+                value={serviceType}
+                onChange={(e) => setServiceType(e.target.value)}
+                label="Select service type"
+                >
+                {serviceTypes.map(type => (
+                    <MenuItem key={type} value={type}>
+                    {type}
+                    </MenuItem>
+                ))}
+                </Select>
+            </FormControl>
+            <FormControl fullWidth>
+                <InputLabel>Status</InputLabel>
+                <Select
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+                label="Select status"
+                >
+                {statuses.map(status => (
+                    <MenuItem key={status} value={status}>
+                    {status}
+                    </MenuItem>
+                ))}
+                </Select>
+            </FormControl>
+            
+            <Box sx={{ display: "flex", justifyContent: "space-between", mt: 2 }}>
+                <Button variant="outlined" onClick={resetToDefault}>
+                Reset to Default
+                </Button>
+                <Button variant="contained" color="primary">
+                Apply
+                </Button>
+            </Box>
+        </Box>
+    </>
+);
 };
 
 export default ProductsPage;
